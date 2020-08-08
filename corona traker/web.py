b2f29253ed74d037c2d81e8834bcbac5e708deae
Amodh Gunawardana <amodh_g@desktop>
2020-08-08 14:06:58 +0530

    make it better

--- corona traker/web.py
+++ corona traker/web.py
@@ -5,11 +5,16 @@ result= requests.get('https://www.worldometers.info/coronavirus/country/sri-lank
 scr= result.content
 soup= BeautifulSoup(scr, 'html.parser')
 number_of_patians=soup.find(class_='maincounter-number').get_text()
-lates_news= soup.find(class_='news_date').get_text()
 count= soup.find_all('span')
 number_of_deaths=str(count[5])
-number_of_deaths= number_of_deaths[6:8]
 number_of_recovered=str(count[6])
-number_of_recovered= number_of_recovered[6:11]
-
+number_of_recovered= number_of_recovered.replace('<span>','')
+number_of_recovered= number_of_recovered.replace('</span>','')
+number_of_deaths= number_of_deaths.replace('<span>','')
+number_of_deaths= number_of_deaths.replace('</span>','')
+lates_news= soup.find(class_='news_li').get_text()
+lates_news= lates_news.replace('[source]', '')
+day= str(soup.find(class_='news_date').get_text())
+lates_news= str(lates_news)+' on the '+day
 # print(lates_news)
+# print(day)
